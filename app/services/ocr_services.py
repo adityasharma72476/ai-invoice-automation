f398e1bbeb54e760c7f5c1f6e0f649875399bc6f
Aditya Sharma <adityasharma72476@gmail.com>
2026-05-01 16:45:34 +0530

    Fix deployment for Linux server

--- app/services/ocr_services.py
+++ app/services/ocr_services.py
@@ -1,8 +1,10 @@
+import os
 import pytesseract
 from PIL import Image
 
-#window path to tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# Set path only on Windows local machine
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 def extract_text_from_image(file_path: str):
